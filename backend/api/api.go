0d4386f4b9b83d772aaade66ac990e68837f3120
Néfix Estrada <nefixestrada@gmail.com>
2021-01-17 11:15:38 +0100

    Fixed api parameters

--- backend/api/api.go
+++ backend/api/api.go
@@ -66,8 +66,8 @@ func New(env *env.Env) *API {
 	a.Mux.HandleFunc("/api/"+version+"/viewer", a.isAuthenticated(a.viewer))
 
 	a.Mux.HandleFunc("/api/"+version+"/desktops", a.isAuthenticated(a.desktops))
-	a.Mux.HandleFunc("/api/"+version+"/desktop/{id}/start", a.isAuthenticated(a.desktopStart))
-	a.Mux.HandleFunc("/api/"+version+"/desktop/{id}/stop", a.isAuthenticated(a.desktopStop))
+	a.Mux.HandleFunc("/api/"+version+"/desktop/{desktop}/start", a.isAuthenticated(a.desktopStart))
+	a.Mux.HandleFunc("/api/"+version+"/desktop/{desktop}/stop", a.isAuthenticated(a.desktopStop))
 
 	return a
 }
